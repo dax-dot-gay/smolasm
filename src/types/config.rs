70c4d3ac2f4c14d0861eef03ed160674f1ee73da
Dax Harris <git@dax.gay>
2026-03-20 12:41:26 -0400

    wip more config

--- src/types/config.rs
+++ src/types/config.rs
@@ -1,9 +1,30 @@
 use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct SystemConfig {
     pub name: String,
     pub format: String,
+    pub format_args: HashMap<String, Value>
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct VariantConfig {
+    pub variant: u64,
+    pub name: String,
+    pub alias: Vec<String>
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub enum FieldType {
+    Enum(HashMap<u64, VariantConfig>),
+    Raw
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct FieldConfig {
+    pub name: String,
+    pub field_type: FieldType
 }
